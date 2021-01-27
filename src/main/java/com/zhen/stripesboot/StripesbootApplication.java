d4f83110a9449d260810a2ccda6f9367e2ea26d7
“zhangchangzhen <304158875@qq.com>
2021-01-27 12:10:44 +0800

    初始化

---
+++ src/main/java/com/zhen/stripesboot/StripesbootApplication.java
@@ -0,0 +1,13 @@
+package com.zhen.stripesboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripesbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StripesbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhen/stripesboot/StripesbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhen.stripesboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripesbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
